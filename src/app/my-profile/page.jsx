623f56f62f5d930f1f38174052c00759a5afbcd4
ashikur rahman <ashikorrahman8900@gmail.com>
2026-05-02 12:35:44 +0600

    added my-profile page and update functionality

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -1,10 +1,33 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button, Card } from "@heroui/react";
+import Link from "next/link";
+
 
 const MyProfilePage = () => {
-    return (
-        <div>
-            My Profile
-        </div>
-    );
+  const userData = authClient.useSession();
+  const user = userData.data?.user;
+
+
+  return (
+    <div>
+      <Card className="max-w-96 mx-auto flex flex-col items-center border my-10">
+        <Avatar className="h-20 w-20">
+          <Avatar.Image
+            alt="John Doe"
+            src={user?.image}
+            referrerPolicy="no-referrer"
+          />
+          <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+        </Avatar>
+
+        <h2 className="text-xl font-bold">{user?.name}</h2>
+        <p className="text-muted">{user?.email}</p>
+        <Link href={"/my-profile/update?callbackUrl=/my-profile"}><Button className="mt-5">Update Profile</Button></Link>
+      </Card>
+    </div>
+  );
 };
 
 export default MyProfilePage;
---
+++ src/app/my-profile/update/page.jsx
@@ -0,0 +1,75 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
+import {
+  Alert,
+  Button,
+  Card,
+  FieldError,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const UpdateProfilePage = () => {
+  const router = useRouter();
+  const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const { data: res, error } = await authClient.updateUser({
+      name: data.name,
+      image: data.image,
+    });
+    if (error) {
+      setMessage(error.message);
+    }
+    if (res) {
+      setMessage("Update successful");
+      router.push("/my-profile");
+    }
+  };
+
+  return (
+    <Card className="max-w-125 mx-auto border py-10 my-10">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex max-w-96 w-full flex-col gap-4 mx-auto"
+      >
+        <h1 className="text-center text-2xl font-bold">Update Profile</h1>
+
+        <TextField isRequired className="w-full" name="fullName">
+          <Label>Name</Label>
+          <Input {...register("name")} placeholder="Enter your name" />
+          <FieldError />
+        </TextField>
+
+        <TextField isRequired name="image" type="text">
+          <Label>Image URL</Label>
+          <Input {...register("image")} placeholder="Image URL" />
+          <FieldError />
+        </TextField>
+        {message && (
+          <Alert variant="solid" color="primary" title="Info">
+            {message}
+          </Alert>
+        )}
+
+        <div className="flex items-center gap-3 mt-5">
+          <Button type="submit">Update</Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default UpdateProfilePage;

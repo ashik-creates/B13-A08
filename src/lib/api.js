784d1535fd8fd67d4118e4e9aee7bbf6c9d5cb4d
ashikur rahman <ashikorrahman8900@gmail.com>
2026-05-02 20:59:20 +0600

    fixes some issues for production

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,5 @@
 export const getAllData = async () => {
-  const res = await fetch("http://localhost:3000/data.json");
+  const res = await fetch("https://b13-a08.vercel.app/data.json");
   const data = await res.json();
   return data;
 };

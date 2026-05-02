8a15859722ea8551a79d7c7cbcfab2ccd7a60a9f
ashikur rahman <ashikorrahman8900@gmail.com>
2026-05-03 01:55:21 +0600

    again trying to fix google auth in prod

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,6 +6,8 @@ const client = new MongoClient(process.env.DB_URI);
 const db = client.db("skill_sphere");
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET, 
+  baseURL: process.env.BETTER_AUTH_URL,
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client,
